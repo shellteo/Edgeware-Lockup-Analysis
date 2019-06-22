ddadb35de7402be11abff05c0192ae57357eb6bf
zxplus <shellteo@163.com>
2019-06-22 14:34:37 +0800

    update

--- back/config/config.local.js
+++ back/config/config.local.js
@@ -6,5 +6,14 @@
  */
 module.exports = () => {
   const config = {};
+  // 配置端口号，主机名
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 7001,
+      hostname: '',
+      https: true,
+    },
+  };
   return config;
 };
